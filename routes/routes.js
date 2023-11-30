8586e433bdd661fddb44720f5186af1892821534
lovistawiah <lovistawiah638@gmail.com>
2023-11-30 03:23:37 +0000

    fixed url error in route.js

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,6 @@ const { signup, login } = require("../controllers/userAccount");
 const { updateProfilePic } = require("../utils/modifyProfilePic");
 router.route("/signup").post(signup);
 router.route("/login").post(login);
-router.route("profile-pic").post(upload.single("image"), updateProfilePic);
+router.route("/profile-pic").post(upload.single("image"), updateProfilePic);
 
 module.exports = router;
