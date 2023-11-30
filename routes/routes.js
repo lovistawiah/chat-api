fd844d0acd58ac1deca6a1ffd5c95ea897159ef2
lovistawiah <lovistawiah638@gmail.com>
2023-11-30 03:12:44 +0000

    adding profile pic feature

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
-const {
-  signup,
-  login,
-} = require("../controllers/userAccount");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const { signup, login } = require("../controllers/userAccount");
+const { updateProfilePic } = require("../utils/modifyProfilePic");
 router.route("/signup").post(signup);
 router.route("/login").post(login);
+router.route("profile-pic").post(upload.single("image"), updateProfilePic);
 
 module.exports = router;
---
+++ utils/modifyProfilePic.js
@@ -0,0 +1,7 @@
+function updateProfilePic(req, res) {
+  const file = req.file;
+
+  console.log(file);
+}
+
+module.exports = { updateProfilePic };
