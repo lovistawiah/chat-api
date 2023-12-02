e7f8c4e79e7c1801ff430bd5dd431e647dce4847
lovistawiah <lovistawiah638@gmail.com>
2023-12-02 12:40:36 +0000

    added Readme file and encryption and decryption features

--- utils/encrypt-decrypt.js
+++ utils/encrypt-decrypt.js
@@ -1,42 +1,21 @@
-const {
-  scrypt,
-  randomFill,
-  createCipheriv,
-  scryptSync,
-  createDecipheriv,
-} = require("node:crypto");
-const { Buffer } = require("node:buffer");
-const algorithm = "aes-192-cbc";
-const password = "Password used to generate key";
+const crypto = require("crypto");
 
-// scrypt(password, "salt", 24, (err, key) => {
-//   if (err) throw err;
-//   // Then, we'll generate a random initialization vector
-//   randomFill(new Uint8Array(16), (err, iv) => {
-//     if (err) throw err;
+const generateKey = () => crypto.randomBytes(32);
+const generateIV = () => crypto.randomBytes(16);
 
-//     const cipher = createCipheriv(algorithm, key, iv);
+// Function to encrypt a message using AES-256
+const encryptMessage = (message, key, iv) => {
+    const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+    let encrypted = cipher.update(message, "utf-8", "hex");
+    encrypted += cipher.final("hex");
+    return encrypted;
+};
 
-//     let encrypted = cipher.update(
-//       "some clear text",
-//       "utf8",
-//       "hex"
-//     );
-//     encrypted += cipher.final("hex");
-//     console.log(encrypted);
-//   });
-// });
+const decryptMessage = (encryptedMessage, key, iv) => {
+    const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+    let decrypted = decipher.update(encryptedMessage, "hex", "utf-8");
+    decrypted += decipher.final("utf-8");
+    return decrypted;
+};
 
-// !
-
-const key = scryptSync(password, "salt", 24);
-// The IV is usually passed along with the ciphertext.
-const iv = Buffer.alloc(16, 0); // Initialization vector.
-
-const decipher = createDecipheriv(algorithm, key, iv);
-
-// Encrypted using same algorithm, key and iv.
-const encrypted = "f6718092156611a7183393924f9f4bb3";
-let decrypted = decipher.update(encrypted, "hex", "utf8");
-decrypted += decipher.final("utf8");
-console.log(decrypted);
+module.exports = { decryptMessage, encryptMessage, generateIV, generateKey };
