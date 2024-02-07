1352d49e062dd43d0549c4975b20b339078ee44b
lovistawiah <lovistawiah638@gmail.com>
2024-02-07 21:50:05 +0000

    changed msgDate to updatedAt and createdAt

--- ioInstance/index.js
+++ ioInstance/index.js
@@ -18,7 +18,7 @@ const {
 
 const io = new Server({
     cors: {
-        origin: ["http://localhost:5173", "http://172.20.10.2:5173"],
+        origin: ["http://172.20.10.2:5173"],
         credentials: true,
     },
 });
