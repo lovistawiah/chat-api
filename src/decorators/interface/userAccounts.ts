4fb134aaba58647199fc16639ab7946adc96e79e
lovistawiah <lovistawiah638@gmail.com>
2024-03-02 11:45:42 +0000

    type checked token.ts

---
+++ src/decorators/interface/userAccounts.ts
@@ -0,0 +1,4 @@
+export interface IUserInfo {
+    userId: string,
+    username: string,
+}
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,18 +1,15 @@
-const jwt = require("jsonwebtoken");
-/**
- * the userInfo object accepts **userId**, **username**
- * @param {} userInfo
- * @returns {string}
- */
-function createToken(userInfo) {
-    const token = jwt.sign(userInfo, process.env.JWT_SECRET, {
+import jwt from 'jsonwebtoken'
+import { IUserInfo } from '../decorators/interface/userAccounts.js';
+
+function createToken(userInfo: IUserInfo) {
+    const token = jwt.sign(userInfo, process.env.JWT_SECRET as string, {
         expiresIn: "30d",
     });
     return token;
 }
 
-function verifyToken(token) {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+function verifyToken(token: string) {
+    const payload = jwt.verify(token, process.env.JWT_SECRET as string);
     if (!payload) {
         const err = new Error("invalid token");
         return err;
