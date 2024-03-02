e0116591de36419f3979bb1c0af980eeb2136681
lovistawiah <lovistawiah638@gmail.com>
2024-03-02 13:10:47 +0000

    type checked models/Chats.ts

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema(
     {
@@ -33,4 +33,4 @@ const chatSchema = new mongoose.Schema(
 );
 const Chat = mongoose.model("chat", chatSchema);
 
-module.exports = Chat;
+export default Chat;
